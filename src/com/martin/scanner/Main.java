e93a944d35c2d7131f230350567fafddf50aa54c
Martin Jernstrom <martin.jernstrom1@gmail.com>
2023-09-19 13:30:56 +0200

    basic Scanner

--- src/com/martin/scanner/Main.java
+++ src/com/martin/scanner/Main.java
@@ -6,43 +6,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-    boolean isPlaying = true;
+    Scanner sc = new Scanner(System.in);
 
     do{
 
+        System.out.println("Input Number!");
+        int result = sc.nextInt();
 
-   int result = scannerNumber();
+        System.out.println("You picked " + result);
 
-        if(result >1000) {
-            isPlaying = false;
-        }else {
-
-            for (int i = 0; i < result; i++) {
-                System.out.println(i);
-                System.out.println("looping...");
-            }
-        }
-
-
-    } while(isPlaying);
-
+    } while (true);
 
     }
 
-    public static String scannerText(){
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Insert sentence");
-        String input = scanner.nextLine();
-
-        return input;
-    }
-    public static int scannerNumber(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("please inster a number:");
-
-
-        return scanner.nextInt();
-    }
 }
