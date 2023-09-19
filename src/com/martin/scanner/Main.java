47006c9fc369abd50ac43648cc8db4ea41b103ed
Martin Jernstrom <martin.jernstrom1@gmail.com>
2023-09-19 13:14:57 +0200

    Scanner prac

--- src/com/martin/scanner/Main.java
+++ src/com/martin/scanner/Main.java
@@ -1,7 +1,48 @@
 package com.martin.scanner;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("app is running");
+
+    boolean isPlaying = true;
+
+    do{
+
+
+   int result = scannerNumber();
+
+        if(result >1000) {
+            isPlaying = false;
+        }else {
+
+            for (int i = 0; i < result; i++) {
+                System.out.println(i);
+                System.out.println("looping...");
+            }
+        }
+
+
+    } while(isPlaying);
+
+
+    }
+
+    public static String scannerText(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insert sentence");
+        String input = scanner.nextLine();
+
+        return input;
+    }
+    public static int scannerNumber(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("please inster a number:");
+
+
+        return scanner.nextInt();
     }
 }
